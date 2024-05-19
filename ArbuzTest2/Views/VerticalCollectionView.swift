19dcd5dfa6252fb1be399dcefb9c4a493f130e1a
Dinmukhamed Altynbek <a.dimash.a_03@mail.ru>
2024-05-19 18:53:28 +0500

    Create product card and collections views

--- ArbuzTest2/Views/VerticalCollectionView.swift
+++ ArbuzTest2/Views/VerticalCollectionView.swift
@@ -5,3 +5,21 @@
 //  Created by Димаш Алтынбек on 19.05.2024.
 //
 
+import SwiftUI
+
+struct VerticalCollectionView: View {
+    var body: some View {
+        LazyVGrid(columns: [GridItem(), GridItem()]) {
+            ForEach(sampleProducts) { product in
+                ProductCardView(product: product)
+            }
+        }
+        .padding()
+    }
+}
+
+struct VerticalCollectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        VerticalCollectionView()
+    }
+}
