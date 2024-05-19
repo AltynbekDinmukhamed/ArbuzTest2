c5d594d52b9948aaa33ad850b9b9c12dca8a9029
Dinmukhamed Altynbek <a.dimash.a_03@mail.ru>
2024-05-19 18:28:06 +0500

    correct MainView

--- ArbuzTest2/Views/MainView.swift
+++ ArbuzTest2/Views/MainView.swift
@@ -5,4 +5,29 @@
 //  Created by Димаш Алтынбек on 19.05.2024.
 //
 
+import SwiftUI
 
+struct MainView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack {
+                    // Header
+                    ProductCardView(product: sampleProduct)
+                        .padding()
+                    
+                    // Collections
+                    HorizontalCollectionView()
+                    VerticalCollectionView()
+                }
+            }
+            .navigationTitle("Главная")
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
