a8041c167e0c0356d53c3782a5b21122475aa875
Dinmukhamed Altynbek <a.dimash.a_03@mail.ru>
2024-05-21 23:34:26 +0500

    .

--- ArbuzTest2/ViewModels/ProductRepository.swift
+++ ArbuzTest2/ViewModels/ProductRepository.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class ProductRepository {
+    static func fetchProduct(by id: UUID, completion: @escaping (Product?) -> Void) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2) {
+            if let savedProducts = UserDefaults.standard.data(forKey: "products"),
+               let decodedProducts = try? JSONDecoder().decode([Product].self, from: savedProducts) {
+                let product = decodedProducts.first(where: { $0.id == id })
+                DispatchQueue.main.async {
+                    completion(product)
+                }
+            } else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+            }
+        }
+    }
+}
